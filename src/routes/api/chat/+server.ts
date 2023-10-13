5b64b85e0a00c32fb369352a433ce5f68cfe9727
Ainekko <ahlaqachhafid@gmail.com>
2023-10-13 18:39:11 +0100

    fucked it up ... let's try

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -182,7 +182,7 @@ export const POST = (async ({ request }) => {
 
     const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
     const currentMessageContent = messages[messages.length - 1].content;
-    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+    //const prompt = PromptTemplate.fromTemplate(TEMPLATE);
     //const body = await request.json();
 
     //const { embeddings, pineconeStore } = await initializeAIComponents();
@@ -207,6 +207,36 @@ export const POST = (async ({ request }) => {
 
     console.log('cntx',context)
 
+    const prompt = {
+      role: "system",
+      content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+      The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+      AI is a well-behaved and well-mannered individual.
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+      AI assistant is a big fan of Pinecone and Vercel.
+      START CONTEXT BLOCK
+      ${context}
+      END OF CONTEXT BLOCK
+      AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+      If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not invent anything that is not drawn directly from the context.
+      `,
+    };
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        prompt,
+        messages.map((message: any) => ({
+          content: message.content,
+          role: message.role,
+        })),
+      ],
+      stream: true,
+    });
+
     const chatModel = new ChatOpenAI({
       streaming:true,
       openAIApiKey: process.env.OPENAI_API_KEY,
@@ -218,19 +248,19 @@ export const POST = (async ({ request }) => {
 
     //const chain = RunnableSequence.from([prompt, chatModel, outputParser]);
 
-    const chain = prompt.pipe(chatModel).pipe(outputParser);  
+    //const chain = prompt.pipe(chatModel).pipe(outputParser);  
   
     
     
     //const stream = OpenAIStream(response);
+    const stream = OpenAIStream(response);
 
-
-    const stream = await chain.stream({
+    // const stream = await chain.stream({
       
-      context  : context,
-      chat_history: formattedPreviousMessages.join('\n'),
-      input: currentMessageContent,
-    });
+    //   context  : context,
+    //   chat_history: formattedPreviousMessages.join('\n'),
+    //   input: currentMessageContent,
+    // });
 
     console.log(stream)
 
