e1814f742cb2d6c3719298dfa8abe7d2cac0e6fd
Ainekko <ahlaqachhafid@gmail.com>
2023-10-14 15:21:25 +0100

    prompt

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -131,25 +131,28 @@ const TEMPLATE = `
 
       START CONTEXT BLOCK
       {context}
+      Current conversation:
+      {chat_history}
+      
+      User: {input}
+      AI:
       END OF CONTEXT BLOCK
-      take into account the CONTEXT BLOCK that is provided in a conversation.
+      Take into account the CONTEXT BLOCK that is provided in a conversation.
       If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
 
       you will not apologize for previous responses, but instead will indicated new information was gained.
       do not invent anything that is not drawn directly from the context.
-      review your answer and make sure it satisfies the user's question {question}
+      
 
       always make the answer short, don't take more than 5 seconds to answer make your answer cohesive and based on the provided context block Then ask if they want to learn more
       always make the answer short, cohesive and based on the provided context block Then ask if they want to learn more
 
       always make your answer short then ask if they want to learn more
       always complete your ideas. don't stop mid sentence
-
-      Current conversation:
-      {chat_history}
+      do not invent anything that is not drawn directly from the context.
+      review your answer and make sure it satisfies the user's question {question}
       
-      User: {input}
-      AI:
+      
       
 
  
