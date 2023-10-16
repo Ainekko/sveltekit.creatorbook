351ec67c632d3baa87bd3201284fa9c9bd40b9c7
Ainekko <ahlaqachhafid@gmail.com>
2023-10-17 00:51:43 +0100

    twetter button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -12,6 +12,38 @@
     if (token){
       goto('/dashboard');
     }
+
+    async function handleTwitterLogin() {
+    try {
+      // Make a request to your Django endpoint responsible for Twitter authentication
+      const response = await fetch('https://api.creatorbook.tech/o/login/twitter', {
+        method: 'POST', // Adjust the method as needed
+      });
+
+      if (response.ok) {
+        // Parse the JSON response data
+        const data = await response.json();
+        
+        if (data.status === 'success') {
+          // User is successfully authenticated
+          // Update the UI or show a success message
+          console.log('Login successful:', data.message);
+        } else {
+          // Handle authentication failure
+          // Display an error message
+          console.error('Authentication failed:', data.message);
+        }
+      } else {
+        // Handle unexpected response status codes
+        // Display an error message
+        console.error('Unexpected response:', response.statusText);
+      }
+    } catch (error) {
+      // Handle any errors that might occur during the authentication process
+      console.error('Error:', error);
+    }
+  }
+
     async function log_in() {
       const user_data = {
         email: email,
@@ -48,7 +80,8 @@
   </script>
   
   <div class="bg-inherit h-screen w-max[1440px] flex justify-center items-center">
-    <a href="https://api.creatorbook.tech/o/login/twitter">LOGIN WITH TWITTER</a>
+    
+    <button on:click={handleTwitterLogin}>Login with Twitter</button>
     <div class="h-screen w-screen max-w-[1200px] flex justify-center items-center overflow-hidden">
       <div class="card bg-dark border flex justify-center items-center border-stone-900 w-80 rounded-[13px] font-mono">
         <p>Log in:</p>
