3c233bfdcfa52cb27104d9215ac3579968c9e826
Ainekko <ahlaqachhafid@gmail.com>
2023-10-17 15:18:16 +0100

    get_token in dashboard

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -2,8 +2,38 @@
     import type { LayoutData } from './$types';
     import NavBar from "$lib/components/NavBar.svelte";
 
+    import { onMount } from 'svelte';
+
     
     export let data: LayoutData;
+
+  // Function to get the token from the backend
+  const getToken = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        // Token not in local storage, fetch it from the backend
+        const response = await fetch('https://api.creatorbook.tech/users/get_token/', {
+          method: 'GET',
+          
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          const fetchedToken = data.token;
+          localStorage.setItem('token', fetchedToken);
+        } else {
+          // Handle error if the token couldn't be fetched
+          console.error('Failed to fetch token:', response.status);
+        }
+      }
+    } catch (error) {
+      console.error('Error while fetching token:', error);
+    }
+  };
+
+  onMount(getToken);
+
 </script>
 
 
