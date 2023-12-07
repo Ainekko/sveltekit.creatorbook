16562000b76c40543dfdd4d21c5a699f0433cd95
Ainekko <ahlaqachhafid@gmail.com>
2023-12-07 17:15:05 +0100

    lj

---
+++ src/routes/lj/+page.svelte
@@ -0,0 +1,194 @@
+<div >
+    <div class="hero w-auto">
+        <div class="flex flex-col md:flex-row w-full">
+            
+            <div class=" w-full ">
+                <img class="object-cover w-f" src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571d8b16e136ab04990e22e_hero%20img.jpg" alt="">
+            </div>
+            <div class="w-full relative flex  justify-center items-center p-5 md:p-0">
+                
+
+                <div class="w-full flex flex-col justify-around items-start gap-2">
+                   <div class="border border-zinc-700 px-4 flex justify-between items-center gap-4 rounded-full max-w-xs">
+
+                    <div class="p-1 bg-pink-500  rounded-full shadow-md shadow-pink-300">
+
+                    </div>
+
+                    <p >
+                        Zara jumpsuit super offer
+                    </p>
+                   </div>
+                    
+
+                    
+                    
+                    <div class=" text-6xl text-white font-bold mb-4">
+                        40% off
+                        
+                    </div>
+
+                    <p class="max-w-xl mb-4 ">
+                        Unleash savings with our exclusive offer! Enjoy a remarkable 40% off on a wide range of premium products. Elevate your experience without breaking the bank. Act now to indulge in quality at an irresistible price. Limited time only – don't miss out on this incredible 40% discount!
+                    </p>
+                    
+                    <button class="btn max-w-xs">
+                        GET offer
+                    </button>
+                </div>
+
+                
+    
+                <img class="absolute -z-10" src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571d8b21de3e9904cb1bcf6_1024px-Zara_Logo%201.png" alt="">
+            </div>
+            
+            
+        </div>
+    </div>
+</div>
+
+<div class="hero w-auto p-5 md:py-40 ">
+    <div class="flex flex-col md:flex-row w-full">
+        
+        <div class=" w-auto mb-5 ">
+            <img class="object-cover w-f" src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571e5731adc384709740a56_photo_5_2023-12-06_18-47-04.jpg" alt="">
+        </div>
+        <div class="w-full relative flex flex-row-reverse justify-center items-center md:px-20">
+            
+
+            <div class="w-full flex flex-col justify-around items-start gap-2">
+               <div class="border border-zinc-700 px-4 flex justify-between items-center gap-4 rounded-full max-w-xs">
+
+                <div class="p-1 bg-pink-500  rounded-full shadow-md shadow-pink-300">
+
+                </div>
+
+                <p >
+                    40% off the 2nd one
+                </p>
+               </div>
+                
+
+                
+                
+                <div class=" text-6xl text-white font-bold mb-4 ">
+                    Get one + 40 off the second
+                    
+                </div>
+
+                <p class="max-w-xl mb-4 ">
+                    describing how the offer works  describing how the offer works  describing how the offer works  describing how the offer works  blah blah blah blah blah blah
+                </p>
+                
+                <button class="btn max-w-xs">
+                    GET offer
+                </button>
+            </div>
+
+            
+
+            <img class="absolute -z-10 opacity-50" src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571d8b21de3e9904cb1bcf6_1024px-Zara_Logo%201.png" alt="">
+        </div>
+        
+        
+    </div>
+</div>
+
+<!-- <div class="w-full flex justify-center items-center text-2xl font-bold">
+
+    <h1>HIGH QUALITY DURABLE MATERIAL</h1>
+
+</div> -->
+
+<section class="flex justify-center pt-10" >
+    <div class="max-w-[1200px]">
+        <div class="w-full flex justify-center items-center text-4xl font-bold md:p-10">
+            <h1>POPULAR PRODUCTS</h1>
+        </div>
+        
+        
+        <div class="grid md:grid-cols-4 grid-cols-1  gap-4 w-full" >
+            
+            <div class="flex flex-col px-10 ">
+
+                <img src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571e5731adc384709740a56_photo_5_2023-12-06_18-47-04.jpg" alt="">
+
+                <div class="justify-between mb-4 text-sm py-5 text-zinc-400">
+                    
+                    <p>
+                       WHATEVER JUMPSUIT 1
+                    </p>
+
+                    <p>
+                        250 MAD
+                    </p>
+    
+                    
+                </div>
+
+            </div>
+
+            <div class="flex flex-col px-10 ">
+
+                <img src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571e5731adc384709740a56_photo_5_2023-12-06_18-47-04.jpg" alt="">
+
+                <div class="justify-between mb-4 text-sm py-5 text-zinc-400">
+                    
+                    <p>
+                       WHATEVER JUMPSUIT 1
+                    </p>
+
+                    <p>
+                        250 MAD
+                    </p>
+    
+                    
+                </div>
+
+            </div>
+
+            <div class="flex flex-col px-10 ">
+
+                <img src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571e5731adc384709740a56_photo_5_2023-12-06_18-47-04.jpg" alt="">
+
+                <div class="justify-between mb-4 text-sm py-5 text-zinc-400">
+                    
+                    <p>
+                       WHATEVER JUMPSUIT 1
+                    </p>
+
+                    <p>
+                        250 MAD
+                    </p>
+    
+                    
+                </div>
+
+            </div>
+
+            <div class="flex flex-col px-10 ">
+
+                <img src="https://uploads-ssl.webflow.com/638ca0b6c42934441d908d49/6571e5731adc384709740a56_photo_5_2023-12-06_18-47-04.jpg" alt="">
+
+                <div class="justify-between mb-4 text-sm py-5 text-zinc-400">
+                    
+                    <p>
+                       WHATEVER JUMPSUIT 1
+                    </p>
+
+                    <p>
+                        250 MAD
+                    </p>
+    
+                    
+                </div>
+
+            </div>
+            
+        
+        </div>
+
+        
+
+    </div>
+</section>
