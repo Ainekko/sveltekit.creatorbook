208efe5ef57bb1e0d3ff83446e700b0f46424301
Ainekko <ahlaqachhafid@gmail.com>
2024-07-15 17:55:34 +0100

    tech startup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,19 @@
 	
 	
   </script>
+
+<svelte:head>
+	<title>Validation as a service for tech startups</title>
+	<meta name="description" content="Get top-tier validation services for your startup. Focus on building while we handle the validation process for you.">
+	<meta property="og:title" content="Validation as a service for upcoming startups">
+	<meta property="og:description" content="Get top-tier validation services for your startup. Focus on building while we handle the validation process for you.">
+	<meta property="og:image" content="https://rechatcreatorbook.s3.us-west-2.amazonaws.com/createrbook+final/new+cb+assets/Home.jpg">
+	<meta property="og:type" content="website">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="Validation as a service for upcoming startups">
+	<meta name="twitter:description" content="Get top-tier validation services for your startup. Focus on building while we handle the validation process for you.">
+	<meta name="twitter:image" content="https://rechatcreatorbook.s3.us-west-2.amazonaws.com/createrbook+final/new+cb+assets/Home.jpg">
+  </svelte:head>
   
   <style>
 
