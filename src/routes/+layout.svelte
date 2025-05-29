f453202ea6922f9df60e357aaf648417dce6fd05
Ainekko <ahlaqachhafid@gmail.com>
2025-05-29 11:15:54 +0100

    trying hog pooost

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
   import "../app.css";
   import Deal from '$lib/components/Deal.svelte';
 	import NavBar from '$lib/components/NavBar.svelte';
+  import { onMount } from 'svelte';
+
   // import { checkAndDeleteToken } from '$lib/check';
+
+  import posthog from 'posthog-js'
+  import { browser } from '$app/environment';
 
 
 
@@ -13,6 +18,17 @@
   // checkAndDeleteToken();
 
   inject();
+  onMount(() => {
+  if (browser) {
+    posthog.init(
+      'phc_JyJ1MIeHjGHDj50rZhIFmLSJlKm9kXjejFtBbKipRk5',
+      {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+      }
+    )
+  }
+});
 </script>
 
 <svelte:head>
