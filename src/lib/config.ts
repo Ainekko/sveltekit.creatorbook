b7fd50a1260402fef69983cf4d539553e53ae1bb
Ainekko <ahlaqachhafid@gmail.com>
2025-10-30 15:03:57 +0100

    prod setup

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,8 @@
 
 
 const DEVURL = 'http://127.0.0.1:8000'
-const PRODURL = 'https://api.s-tierproject.online'
-const BASEURL  = DEVURL
+const PRODURL = 'https://api.flowjoy.online'
+const BASEURL  = PRODURL
 
 const WORKERDEV = "http://127.0.0.1:8001"
 const WORKERPROD = "https://praw-hell-cry.vercel.app"
