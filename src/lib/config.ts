53cedc00369b3c9cd8003349c4e4d89fe612e15c
Ainekko <ahlaqachhafid@gmail.com>
2025-11-23 16:52:12 +0100

    blog post markdwon tables fix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,7 @@
 
 const DEVURL = 'http://127.0.0.1:8000'
 const PRODURL = 'https://api.flowjoy.online'
-const BASEURL = DEVURL
+const BASEURL = PRODURL
 
 const WORKERDEV = "http://127.0.0.1:8001"
 const WORKERPROD = "https://praw-hell-cry.vercel.app"
